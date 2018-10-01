43386a82307646444234a077d77d70daf79469ec
Małgorzata Osinska <m.osinska2017@gmail.com>
2018-10-01 21:06:20 +0200

    poprawka skasowanie x

--- js/main.js
+++ js/main.js
@@ -1,27 +1,24 @@
 var liczba1 = 2;
 var liczba2 = 3;
 
-function dzialania() {
-    
+function dzialania(liczba1, liczba2) {
+
     var dodawanie = liczba1 + liczba2;
-    var x = dodawanie;
-    if (x > 0 === true) {
+    if (dodawanie > 0 === true) {
         console.log("Wynik dodawania wynosi " + dodawanie);
     } else {
         console.log("Wynik jest ujemny");
     }
-    
+
     var odejmowanie = liczba1 - liczba2;
-    var x = odejmowanie;
-    if (x > 0 === true) {
+    if (odejmowanie > 0 === true) {
         console.log("Wynik odejmowanie wynosi " + odejmowanie);
     } else {
         console.log("Wynik jest ujemny");
     }
-   
+
     var mnozenie = liczba1 * liczba2;
-    var x = mnozenie;
-    if (x > 0 === true) {
+    if (mnozenie > 0 === true) {
         console.log("Wynik mnozenia wynosi " + mnozenie);
     } else {
         console.log("Wynik jest ujemny");
